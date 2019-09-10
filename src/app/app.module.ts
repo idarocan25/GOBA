3e0b70eff402a163251627a3b0f7b2aa6f73562d
ivonne.aroca <ivonne.aroca@softwareestrategico.co>
2019-09-10 10:24:32 -0500

    commit de todos los archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Dependencia para alertar
+import { ToastrModule } from 'ngx-toastr'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//
 import { AppComponent } from './app.component';
+import { NavegationComponent } from './Component/navegation/navegation.component';
+import { HomePrincipalComponent } from './Component/home-principal/home-principal.component';
+// Dependecias para la modal//
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// Dependecia para utilizar ngmodel en los formularios
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegationComponent,
+    HomePrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BootstrapModalModule,
+    NgbModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
